64467cd559755d089f1810550892246bd0b922ce
missxa <aliona.kharchenko@gmail.com>
2021-02-16 20:22:35 +0100

    shipping

--- src/autostart-brain.sh
+++ src/autostart-brain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+nmap -sn 192.168.1.0/24
 raspberrypi=$(arp -n | grep -w -i 'B8:27:EB:B0:E7:9C' | awk '{print $1}')
 fpga=$(arp -n | grep -w -i '00:00:F3:BE:EF:01' | awk '{print $1}')
 projector=$(arp -n | grep -w -i '00:0a:cd:32:ae:b7' | awk '{print $1}')
@@ -15,7 +16,7 @@ tmux new-session -s roboy -d \; \
   send-keys -t 2 'node-red' C-m\; \
   send-keys -t 3 'websocket --wait' C-m\; \
   send-keys -t 4 'face' C-m\; \
-  send-keys -t 5 "ssh root@$fpga -t \"bash -lic  'plexus' \"" C-m\; \
+  send-keys -t 5 "ssh root@$fpga -t \"bash -lic 'plexus' \"" C-m\; \
   send-keys -t 0 'brain-kindyn --wait' C-m\; \
   select-pane -t 2 \; \
   split-window -v \; \
