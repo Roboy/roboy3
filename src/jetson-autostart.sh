3968b8c9908d3f7ec436250812a6b49d7fc11528
missxa <aliona.kharchenko@gmail.com>
2022-11-25 19:54:03 +0100

    bash helper scripts

--- src/jetson-autostart.sh
+++ src/jetson-autostart.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 nmap -sn 192.168.1.0/24
 
-#nmap -sn 10.180.1.0/24
-
 raspberrypi=$(arp -n | grep -w -i 'b8:27:eb:ab:aa:26' |  awk 'NR == 1' | awk '{print $1}') #arp -n | grep -w -i 'b8:27:eb:ab:aa:26' | awk '{print $1}')
 fpga=$(arp -n | grep -w -i '00:00:F3:BE:EF:02' | awk '{print $1}')
-projector=$(arp -n | grep -w -i '00:0e:c6:ac:ca:2b' | awk '{print $1}')
-
-echo $fpga
-echo $projector
-
-/home/roboy/kill_nodered.sh
+projector=$(arp -n | grep -w -i 'cc:4b:73:b5:4b:da' | awk '{print $1}')
 
 tmux list-sessions | awk 'BEGIN{FS=":"}{print $1}' | xargs -n 1 tmux kill-session -t
 tmux new-session -s roboy -d \; \
@@ -26,13 +19,13 @@ tmux new-session -s roboy -d \; \
   send-keys -t 3 'animus' C-m\; \
   send-keys -t 4 'websocket --wait' C-m\; \
   send-keys -t 5 'face' C-m\; \
+  send-keys -t 6 "ssh root@$fpga -t \"bash -lic 'plexus' \"" C-m\; \
   send-keys -t 0 'rosrun rosserial_python serial_node.py tcp' C-m\; \
-  send-keys -t 1 '/home/roboy/run_nodered.sh' C-m \; \
-  send-keys -t 2 /home/roboy/restart_face.sh C-m \;\
-  send-keys -t 6 'ik' C-m\; 
+  send-keys -t 1 'node-red' C-m \; \
+  send-keys -t 2 /home/roboy/restart_face.sh C-m \;
   #send-keys -t 7 'wheel-commander' C-m\; \
   #select-pane -t 5\; \
   #split-window -v \; \
   #send-keys "ssh pi@$raspberrypi -t \"bash -lic 'leds' \"" C-m\;   
-  #"ssh root@$fpga -t \"bash -lic 'plexus' \"" C-m\; \
-#tmux attach
+
+tmux attach
